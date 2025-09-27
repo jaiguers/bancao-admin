64087bc60eba809c65660e00452138cca902882d
Jair Guerrero <jaiguers@outlook.com>
2025-09-27 17:30:51 -0500

    [UPDATE] - Ajustar cambios

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -54,7 +54,7 @@ export default function Sidebar({ isOpen, onToggle }: SidebarProps) {
         />
       )}
 
-      {/* Sidebar */}
+      {/*   Sidebar   */}
       <div className={`
         fixed top-0 left-0 h-full w-64 bg-white shadow-xl transform transition-transform duration-300 ease-in-out z-50
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
